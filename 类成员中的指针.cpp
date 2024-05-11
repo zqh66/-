21d6fa9cb214e8e9ef8b8e5bcb36e879c737e58d
zqh66 <147631568+zqh66@users.noreply.github.com>
2024-05-11 21:27:02 +0800

    Add files via upload

---
+++ 类成员中的指针.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+
+class Data {
+public:
+	typedef unsigned int index;
+	char get() const;
+	char get(index st, index eb);
+	string content="sss";
+	index cursor, top, bottom;
+};
+string Data::* ps = &Data::content;
+
+int main()
+{
+	cout <<ps<< endl;//1
+	//cout << *ps << endl;//错误
+    char(Data:: * pmf)()const = &Data::get;
+	
+	Data dd;
+	string*ps2 = &(dd.content);
+	cout << (*ps2) << endl;//sss
+	//char (*f)(int st, int eb) = &Data::get;
+	
+
+
+}
+
